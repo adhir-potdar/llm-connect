5d287ecd45d9627fa8dbfe62d18f758e17d78aac
Adhir Potdar <adhir.potdar@isanasystems.com>
2024-10-29 08:17:58 +0530

    Testing gpt4 and anthropic connectors

---
+++ anthropic/test.py
@@ -0,0 +1,41 @@
+import anthropic
+
+# Set your Anthropic API key here
+isana_api_key = 'isana_api_key'
+
+# Create an Anthropic client
+client = anthropic.Anthropic(api_key=isana_api_key)
+
+# Define the prompt you want to send to Claude 3.5
+human_input = "What are the NoSQL options to store JSON documents?"
+messages = [
+    {"role": "user", "content": human_input}
+]
+
+# Call the Anthropic API to get a response from Sonet 3.5 using the Messages API
+#message = client.messages.create(
+#    model="claude-3-5-sonnet-20240620",
+#    max_tokens=1024,
+#    messages=messages,
+#    system="You are an experienced data engineer.",
+#    stop_sequences=["The end."],
+#    temperature=0.2
+#)
+#
+#print(message.content)
+response_message = ''
+with client.messages.stream(
+#            model="claude-3-5-sonnet-20240620",
+            model="claude-3-5-sonnet-20241022",
+            max_tokens=4096,
+            messages=messages,
+            system="You are an experienced data engineer.",
+            stop_sequences=["The end."],
+            temperature=0.2
+        ) as stream:
+            for text in stream.text_stream:
+                response_message += text
+                print(text)
+
+print(response_message)
+
---
+++ gpt4/test.py
@@ -0,0 +1,51 @@
+
+import os
+import time
+from openai import OpenAI
+
+# Set your Anthropic API key here
+isana_api_key = 'isana_api_key'
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=isana_api_key,
+)
+
+start_time = time.time()
+
+#chat_completion = client.chat.completions.create(
+response = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": "You are an experienced data engineer."},
+        {
+            "role": "user",
+            "content": "What are the NoSQL options to store JSON documents?"
+        }
+    ],
+    model="gpt-4o-2024-08-06",
+#    stop=["\n", "End of response"],
+    temperature=0.2,
+    stream=True
+)
+
+#print(chat_completion.choices[0].message)
+
+# create variables to collect the stream of chunks
+collected_chunks = []
+collected_messages = []
+# iterate through the stream of events
+for chunk in response:
+    chunk_time = time.time() - start_time  # calculate the time delay of the chunk
+    collected_chunks.append(chunk)  # save the event response
+    chunk_message = chunk.choices[0].delta.content  # extract the message
+    collected_messages.append(chunk_message)  # save the message
+    #print(f"Message received {chunk_time:.2f} seconds after request: {chunk_message}")  # print the delay and text
+
+# print the time delay and text received
+print(f"Full response received {chunk_time:.2f} seconds after request")
+
+# clean None in collected_messages
+collected_messages = [m for m in collected_messages if m is not None]
+full_reply_content = ''.join(collected_messages)
+print(f"Full conversation received: {full_reply_content}")
+
